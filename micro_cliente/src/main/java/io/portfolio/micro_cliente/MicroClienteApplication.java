8b16a60f366a9b5d68eb987bf4998b53eba5fe79
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-11-18 18:53:32 -0400

    Add Microservice_Client

---
+++ micro_cliente/src/main/java/io/portfolio/micro_cliente/MicroClienteApplication.java
@@ -0,0 +1,13 @@
+package io.portfolio.micro_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroClienteApplication.class, args);
+	}
+
+}
---
+++ micro_cliente/src/test/java/io/portfolio/micro_cliente/MicroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.portfolio.micro_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
