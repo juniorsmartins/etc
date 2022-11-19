4fbe960df080785135bcb83977469ad987a663ec
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-11-19 17:46:20 -0400

    Add Policy of DTOs and DTOs.

---
+++ micro_cliente/src/main/java/io/portfolio/micro_cliente/client/domain/dtos/ClientDTORequest.java
@@ -0,0 +1,56 @@
+package io.portfolio.micro_cliente.client.domain.dtos;
+
+import io.portfolio.micro_cliente.client.domain.enums.GenreEnum;
+import io.portfolio.micro_cliente.client.domain.enums.SexEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public final class ClientDTORequest implements Serializable, PolicyDTO<Long> {
+
+    private Long id;
+
+    @NotBlank
+    @Length(max = 40)
+    private String firstName;
+
+    @NotBlank
+    @Length(max = 40)
+    private String lastName;
+
+    @NotBlank
+    @Length(max = 15)
+    @CPF
+    private String cpf;
+
+    @NotBlank
+    @Length(max = 1)
+    private SexEnum sex;
+
+    @NotBlank
+    @Length(max = 30)
+    private GenreEnum genre;
+
+    @NotNull
+    @Past
+    private LocalDate birthDate;
+
+    @NotBlank
+    @Length(max = 30)
+    private String maritalStatus;
+
+    @NotBlank
+    @Length(max = 200)
+    private String education;
+}
---
+++ micro_cliente/src/main/java/io/portfolio/micro_cliente/client/domain/dtos/ClientDTOResponse.java
@@ -0,0 +1,28 @@
+package io.portfolio.micro_cliente.client.domain.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.portfolio.micro_cliente.client.domain.enums.GenreEnum;
+import io.portfolio.micro_cliente.client.domain.enums.SexEnum;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public final class ClientDTOResponse implements Serializable, PolicyDTO<Long> {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String cpf;
+    private SexEnum sex;
+    private GenreEnum genre;
+    private LocalDate birthDate;
+    private String maritalStatus;
+    private String education;
+}
