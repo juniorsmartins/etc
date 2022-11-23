bb14b7e1bb7e7f03718f3868af06c8ea70a3f9f4
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-11-23 18:14:44 -0400

    Added error handling for Bean Validation.

--- micro_cliente/src/main/java/io/portfolio/micro_cliente/shared/exceptions/ExceptionHandling.java
+++ micro_cliente/src/main/java/io/portfolio/micro_cliente/shared/exceptions/ExceptionHandling.java
@@ -2,11 +2,17 @@ package io.portfolio.micro_cliente.shared.exceptions;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public final class ExceptionHandling {
@@ -14,14 +20,28 @@ public final class ExceptionHandling {
     @Autowired
     private MessageSource internationalMessage;
 
-    @ExceptionHandler(ResourceNotFoundCustomException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<StandardExceptionHandledReturn> methodStandardExceptionHandledReturn(
-            ResourceNotFoundCustomException resourceNotFoundCustomException) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<StandardExceptionHandledReturn> methodArgumentNotValidException(MethodArgumentNotValidException method) {
+        List<StandardExceptionHandledReturn> listOfHandledErrors = new ArrayList<>();
+        List<FieldError> listOfFieldErrors = method.getBindingResult().getFieldErrors();
+        listOfFieldErrors.forEach(theError -> {
+            String message = internationalMessage.getMessage(theError, LocaleContextHolder.getLocale());
+            StandardExceptionHandledReturn exceptionHandledReturn = new StandardExceptionHandledReturn(
+                    HttpStatus.BAD_REQUEST.toString(), message, theError.getCode(), theError.getField());
+            listOfHandledErrors.add(exceptionHandledReturn);
+        });
         return ResponseEntity
                 .badRequest()
-                .body(new StandardExceptionHandledReturn(HttpStatus.NOT_FOUND.toString(),
-                        resourceNotFoundCustomException.getMessage()));
+                .body(listOfHandledErrors.get(0));
+    }
+
+    @ExceptionHandler(ResourceNotFoundCustomException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<StandardExceptionHandledReturn> methodStandardExceptionHandledReturn(ResourceNotFoundCustomException resource) {
+        return ResponseEntity
+                .badRequest()
+                .body(new StandardExceptionHandledReturn(HttpStatus.NOT_FOUND.toString(), resource.getMessage()));
     }
 
 }
--- micro_cliente/src/main/java/io/portfolio/micro_cliente/shared/exceptions/StandardMessage.java
+++ micro_cliente/src/main/java/io/portfolio/micro_cliente/shared/exceptions/StandardMessage.java
@@ -2,5 +2,5 @@ package io.portfolio.micro_cliente.shared.exceptions;
 
 public final class StandardMessage {
 
-    public static final String RESOURCE_NOT_FOUND_EXCEPTION = "Recurso não encontrado no banco de dados!";
+    public static final String RESOURCE_NOT_FOUND_EXCEPTION = "Resource not found in database!";
 }
