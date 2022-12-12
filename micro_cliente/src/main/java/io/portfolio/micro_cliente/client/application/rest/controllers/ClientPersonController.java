d4c1c09e7e672518a42271e79e39bb05d8074e27
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-12 18:26:14 -0400

    Added documentation in Person controller.

--- micro_cliente/src/main/java/io/portfolio/micro_cliente/client/application/rest/controllers/ClientPersonController.java
+++ micro_cliente/src/main/java/io/portfolio/micro_cliente/client/application/rest/controllers/ClientPersonController.java
@@ -5,6 +5,11 @@ import io.portfolio.micro_cliente.client.domain.dtos.ClientPersonDTORequest;
 import io.portfolio.micro_cliente.client.domain.dtos.ClientPersonDTOResponse;
 import io.portfolio.micro_cliente.client.domain.filter.ClientPersonFilter;
 import io.portfolio.micro_cliente.client.domain.services.PolicyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +28,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping(value = "${app.api.base}/clients/persons", produces = {"application/json"})
+@Tag(name = "Controller ClientPerson")
 public final class ClientPersonController extends PolicyControllers<ClientPersonDTORequest, ClientPersonFilter, ClientPersonDTOResponse, Long> {
 
     private static Logger log = LoggerFactory.getLogger(ClientPersonController.class);
@@ -30,8 +36,17 @@ public final class ClientPersonController extends PolicyControllers<ClientPerson
     @Autowired
     private PolicyService<ClientPersonDTORequest, ClientPersonFilter, ClientPersonDTOResponse, ClientPersonEntity, Long> service;
 
+    @Operation(summary = "Create", description = "register resource in database.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created - successful request and new resource created."),
+            @ApiResponse(responseCode = "400", description = "Bad Request - request with invalid syntax."),
+            @ApiResponse(responseCode = "409", description = "Conflict - business rule violated."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error - server in unforeseen situation.")
+    })
     @Override
-    public ResponseEntity<ClientPersonDTOResponse> create(@RequestBody @Valid ClientPersonDTORequest dto) {
+    public ResponseEntity<ClientPersonDTOResponse> create(
+            @Parameter(name = "ClientPersonDTORequest", description = "structure for transporting data.", required = true)
+            @RequestBody @Valid ClientPersonDTORequest dto) {
         log.info("Started resource record control.");
 
         var response = this.service.create(dto);
@@ -42,8 +57,17 @@ public final class ClientPersonController extends PolicyControllers<ClientPerson
                 .body(response);
     }
 
+    @Operation(summary = "Update", description = "update resource in database.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - successful request."),
+            @ApiResponse(responseCode = "400", description = "Bad Request - request with invalid syntax."),
+            @ApiResponse(responseCode = "404", description = "Not Found - resource not found in database."),
+            @ApiResponse(responseCode = "409", description = "Conflict - business rule violated.")
+    })
     @Override
-    public ResponseEntity<ClientPersonDTOResponse> update(@RequestBody @Valid ClientPersonDTORequest dto) {
+    public ResponseEntity<ClientPersonDTOResponse> update(
+            @Parameter(name = "ClientPersonDTORequest", description = "structure for transporting data.", required = true)
+            @RequestBody @Valid ClientPersonDTORequest dto) {
         log.info("Started resource update control.");
 
         var response = this.service.update(dto);
@@ -54,8 +78,16 @@ public final class ClientPersonController extends PolicyControllers<ClientPerson
                 .body(response);
     }
 
+    @Operation(summary = "SearchById", description = "search a resource in the database by identifier.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - successful request."),
+            @ApiResponse(responseCode = "400", description = "Bad Request - request with invalid syntax."),
+            @ApiResponse(responseCode = "404", description = "Not Found - resource not found in database.")
+    })
     @Override
-    public ResponseEntity<ClientPersonDTOResponse> searchById(@PathVariable(value = "id") Long id) {
+    public ResponseEntity<ClientPersonDTOResponse> searchById(
+            @Parameter(name = "id", description = "identifying key", example = "22", required = true)
+            @PathVariable(value = "id") Long id) {
         log.info("Started resource fetch control by identifier.");
 
         var response = this.service.searchById(id);
@@ -66,8 +98,15 @@ public final class ClientPersonController extends PolicyControllers<ClientPerson
                 .body(response);
     }
 
+    @Operation(summary = "SearchAll", description = "search all resources from the database.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - successful request."),
+            @ApiResponse(responseCode = "400", description = "Bad Request - request with invalid syntax.")
+    })
     @Override
-    public ResponseEntity<Page<ClientPersonDTOResponse>> searchAll(ClientPersonFilter filter,
+    public ResponseEntity<Page<ClientPersonDTOResponse>> searchAll(
+            @Parameter(name = "ClientPersonFilter", description = "dynamic search by parameters", required = false) ClientPersonFilter filter,
+            @Parameter(name = "Pageable", description = "search with custom pagination", required = false)
        @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable pagination) {
         log.info("Started search control all resources.");
 
@@ -79,8 +118,16 @@ public final class ClientPersonController extends PolicyControllers<ClientPerson
                 .body(response);
     }
 
+    @Operation(summary = "DeleteById", description = "delete resource from database by identifier.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - successful request."),
+            @ApiResponse(responseCode = "400", description = "Bad Request - request with invalid syntax."),
+            @ApiResponse(responseCode = "404", description = "Not Found - resource not found in database.")
+    })
     @Override
-    public ResponseEntity<String> deleteById(@PathVariable(value = "id") Long id) {
+    public ResponseEntity<String> deleteById(
+            @Parameter(name = "id", description = "identifying key", example = "22", required = true)
+            @PathVariable(value = "id") Long id) {
         log.info("Started resource deletion control by identifier.");
 
         var response = this.service.deleteById(id);
