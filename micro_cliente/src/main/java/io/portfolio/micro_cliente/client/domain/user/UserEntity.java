e7523ea104d56666469578442d79994844065868
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-16 17:12:12 -0400

    Added dependencies in Pom and created User entity.

---
+++ micro_cliente/src/main/java/io/portfolio/micro_cliente/client/domain/user/UserEntity.java
@@ -0,0 +1,28 @@
+package io.portfolio.micro_cliente.client.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public final class UserEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login", length = 100, nullable = false)
+    private String login;
+
+    @Column(name = "password", length = 250, nullable = false)
+    private String password;
+}
