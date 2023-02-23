67ae41f4a01ef06a701e3a57a428b1e0566de180
Junior Martins <juniorsoaresmartins@gmail.com>
2023-02-23 07:48:40 -0400

    Producer Kadka

--- teste_kafka/teste_kafka/src/main/java/io/teste_kafka/TesteKafkaApplication.java
+++ teste_kafka/teste_kafka/src/main/java/io/teste_kafka/TesteKafkaApplication.java
@@ -9,5 +9,5 @@ public class TesteKafkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TesteKafkaApplication.class, args);
 	}
-
 }
+
--- teste_kafka/teste_kafka/src/main/java/io/teste_kafka/producer/CarProducer.java
+++ teste_kafka/teste_kafka/src/main/java/io/teste_kafka/producer/CarProducer.java
@@ -1,8 +1,6 @@
 package io.teste_kafka.producer;
 
 import io.teste_kafka.controller.CarDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,7 +8,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 @Service
 public class CarProducer {
 
-    private static final Logger log = LoggerFactory.getLogger(CarProducer.class);
     private final String topic;
     private final KafkaTemplate<String, CarDto> kafkaTemplate;
 
@@ -21,8 +18,8 @@ public class CarProducer {
 
     public void send(CarDto carDto) {
         kafkaTemplate.send(topic, carDto).addCallback(
-                success -> log.info("Mensagem enviada." + success.getProducerRecord().value()),
-                failure -> log.info("Mensagem não enviada." + failure.getMessage())
+                success -> System.out.println("Mensagem enviada." + success.getProducerRecord().value()),
+                failure -> System.out.println("Mensagem não enviada." + failure.getMessage())
         );
     }
 }
